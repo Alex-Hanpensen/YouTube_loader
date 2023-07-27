9b81e88220b45a7dc53c67f11b414d41f7544d82
hanpensen <hanpensen90@mail.ru>
2023-07-27 11:55:49 +0300

    Added the ability to download video in class Loader, added docstring for Loader, added video download test in executable code block

--- youtube_loader.py
+++ youtube_loader.py
@@ -5,6 +5,7 @@ This script is required to upload video clips, playlist and captions to YouTube
 
 import argparse
 from argparse import Namespace
+import re
 import os
 from pytube import YouTube, Playlist
 
@@ -23,6 +24,66 @@ def get_args() -> Namespace:
     return parser.parse_args()
 
 
+class Loader:
+    """
+    YouTube download user argument controller
+
+    Attributes:
+        link (str): Link to YouTube video or playlist  | https://www.youtube.com/...
+        path (str): Output path for saving video clips | Absolute or relative path
+        resolution (str): Video clip quality           | '720p', '480p', '360p', '240p', '144p'
+        captions (str): Captions for uploading video   | 'en', 'ru', 'esp' etc
+
+    Methods:
+        load(self) -> None: Download a video(s) from the specified URL
+    """
+
+    __slots__ = '__link', '__path', '__resolution', '__captions'
+
+    def __init__(self, link, path='.', resolution='720p', captions=None):
+        self.__link = link
+        self.__path = path
+        self.__resolution = resolution
+        self.__captions = captions
+
+    @property
+    def link(self):
+        return self.__link
+
+    @link.setter
+    def link(self, value):
+        self.__link = value
+
+    @property
+    def path(self):
+        return self.__path
+
+    @path.setter
+    def path(self, value):
+        self.__path = value
+
+    @property
+    def resolution(self):
+        return self.__resolution
+
+    @resolution.setter
+    def resolution(self, value):
+        if value in ('720p', '480p', '360p', '240p', '144p'):
+            self.__resolution = value
+
+    def load(self) -> None:
+        video = YouTube(self.__link).streams.filter(resolution=self.__resolution, )
+        video.first().download(self.__path)
+        print('Your video has been successfully uploaded!')
+
+    def __is_playlist(self) -> bool:
+        ...
+
+    def __is_video(self) -> bool:
+        ...
+
+
 if __name__ == '__main__':
     args = get_args()
-
+    loader = Loader(args.link, args.path, args.resolution, args.captions)
+    loader.load()
