3d2d77a518c6c78933d1a5590da4e6bca0219c40
hanpensen <hanpensen90@mail.ru>
2023-07-28 18:59:53 +0300

    Implemented the methods: (__is_video, __is_playlist) wrote a setter for link, removed subtitles from the parameters and the Loader class

--- youtube_loader.py
+++ youtube_loader.py
@@ -19,8 +19,6 @@ def get_args() -> Namespace:
     parser.add_argument('--link', '-l', help='Link to the video', required=True)
     parser.add_argument('--path', '-p', help='The way to save the video', default='.')
     parser.add_argument('--resolution', '-r', help='Video quality', default='720p')
-    parser.add_argument('--captions', '-c', help='Presence or absence of captions', default=None)
-
     return parser.parse_args()
 
 
@@ -32,35 +30,37 @@ class Loader:
         link (str): Link to YouTube video or playlist  | https://www.youtube.com/...
         path (str): Output path for saving video clips | Absolute or relative path
         resolution (str): Video clip quality           | '720p', '480p', '360p', '240p', '144p'
-        captions (str): Captions for uploading video   | 'en', 'ru', 'esp' etc
 
     Methods:
         load(self) -> None: Download a video(s) from the specified URL
     """
 
-    __slots__ = '__link', '__path', '__resolution', '__captions'
+    __slots__ = '__link', '__path', '__resolution'
 
-    def __init__(self, link, path='.', resolution='720p', captions=None):
-        self.__link = link
+    def __init__(self, link, path='.', resolution='720p'):
+        self.link = link
         self.__path = path
         self.__resolution = resolution
-        self.__captions = captions
 
     @property
     def link(self):
         return self.__link
 
     @link.setter
-    def link(self, value):
-        self.__link = value
+    def link(self, url_link: str) -> None:
+        self.__link = url_link if self.__is_video(url_link) or self.__is_playlist(url_link) else print(
+            'Incorrect link to a youtube video')
 
     @property
     def path(self):
         return self.__path
 
     @path.setter
-    def path(self, value):
-        self.__path = value
+    def path(self, path):
+        if not os.path.isdir(path):
+            print('Unable to access the specified directory!')
+        else:
+            self.__path = path
 
     @property
     def resolution(self):
@@ -72,18 +72,20 @@ class Loader:
             self.__resolution = value
 
     def load(self) -> None:
-        video = YouTube(self.__link).streams.filter(resolution=self.__resolution, )
+        video = YouTube(self.__link).streams.filter(resolution=self.__resolution)
         video.first().download(self.__path)
         print('Your video has been successfully uploaded!')
 
-    def __is_playlist(self) -> bool:
-        ...
+    @staticmethod
+    def __is_playlist(link: str) -> bool | None:
+        return re.search(r'"https?://(www.)?youtube\.com/playlist\?list=\w+"gm', link)
 
-    def __is_video(self) -> bool:
-        ...
+    @staticmethod
+    def __is_video(link: str) -> bool | None:
+        return re.search(r"https?://(www.)?youtube\.com/watch\?v=\w+(?:$|&list=\w+&index=\d)", link)
 
 
 if __name__ == '__main__':
     args = get_args()
-    loader = Loader(args.link, args.path, args.resolution, args.captions)
+    loader = Loader(args.link, args.path, args.resolution)
     loader.load()
